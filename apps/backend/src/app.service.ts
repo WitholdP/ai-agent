0cce28dd18b1f5eb23488fb865d33be9fbb0e913
Witold Piecewicz <w.piecewicz@gmail.com>
2025-12-20 15:52:08 +0100

    finished initial setup of the repo with sorting etc

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getRootRoute(): string {
-    return 'Root route api health check';
-  }
+    getRootRoute(): string {
+        return 'Root route api health check';
+    }
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,14 @@
-import { User } from "@repo/application-types/business-types/user";
+import { User } from '@repo/application-types/business-types/user';
 
 export default function Home() {
-  const user: User = {
-    id: "1",
-    name: "John Doe",
-  };
+    const user: User = {
+        id: '1',
+        name: 'John Doe',
+    };
 
-  return (
-    <div>
-      <h1>Hello {user.name}</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Hello {user.name}</h1>
+        </div>
+    );
 }
