0aadf23e71d4dc2c941c690b713a8055d8f97b83
Witold Piecewicz <w.piecewicz@gmail.com>
2025-12-29 11:04:32 +0100

    added clerc to the application

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,8 @@
 import { Button } from '@/components/ui/button';
-import { User } from '@repo/application-types/business-types/user';
 
 export default function Home() {
-    const user: User = {
-        id: '1',
-        name: 'John Doe',
-    };
-
     return (
         <div>
-            <h1>Hello {user.name}</h1>
             <Button>Click me</Button>
         </div>
     );
